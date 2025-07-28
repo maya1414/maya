25ed1bf3450da8122e15fbf617da8befdfecf9ad
maya <maya.romm0401@gmail.com>
2025-07-29 00:55:54 +0300

    6

---
+++ classes/Shirt.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Shirt {
+
+    public int sizeOfShirt;
+    public String colorOfShirt;
+
+    public Shirt(int sizeOfShirt, String colorOfShirt) {
+        this.colorOfShirt = colorOfShirt;
+        this.sizeOfShirt = sizeOfShirt;
+    }
+}
