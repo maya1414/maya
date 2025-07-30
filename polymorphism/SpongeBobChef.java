7485fa5f7580244706009f2c2d3a257097f3db0f
maya <maya.romm0401@gmail.com>
2025-07-30 23:45:38 +0300

    8

--- polymorphism/SpongeBobChef.java
+++ polymorphism/SpongeBobChef.java
@@ -4,6 +4,7 @@ public class SpongeBobChef extends SpongeBob {
 
     public SpongeBobChef(String name, String favoriteFood, int age) {
         super(name, favoriteFood, age);
+        super.playBasketBall();
     }
 
     @Override
