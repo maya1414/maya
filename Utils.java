3103d4c4bdef9961e11198fbed9774506af4a291
maya <maya.romm0401@gmail.com>
2025-07-31 13:08:35 +0300

    fix 4

--- Utils.java
+++ Utils.java
@@ -1,7 +1,13 @@
 public class Utils {
 
-    public int daysBefore(Month month, int dayInMonth){
-        return Month.returnNumOfDays(month) - dayInMonth;
+    public int daysBeforeInYear(Month month, int dayInMonth) {
+        int totalDays = 0;
+        int index = 0;
+        while (Month.values()[index] != month) {
+            totalDays += Month.values()[index].days;
+            index++;
+        }
+        return totalDays + (dayInMonth - 1);
     }
 
 }
