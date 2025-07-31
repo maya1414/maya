43d998ec28884d27b1a36c7d57aef9b0187b4ab9
maya <maya.romm0401@gmail.com>
2025-07-31 23:11:35 +0300

    6

--- exceptions/exceptions.java
+++ exceptions/exceptions.java
@@ -56,7 +56,26 @@ public class exceptions {
         } catch (BindException e) {
             System.out.println(2);
         } catch (AuthenticationException e) {
-            System.out.println(3);        }
+            System.out.println(3);
+        }
+    }
+
+    static void catchExceptionAndPrintNum2(int num) throws FileNotFoundException, AuthenticationException, BindException {
+        try {
+            numbersBetweenOneAndThreeOrNothing(num);
+            throw new RuntimeException();
+        } catch (FileNotFoundException e) {
+            System.out.println(1);
+            throw e;
+        } catch (BindException e) {
+            System.out.println(2);
+            throw e;
+        } catch (AuthenticationException e) {
+            System.out.println(3);
+            throw e;
+        }finally {
+            System.out.println("here");
+        }
     }
 
 }
