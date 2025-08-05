6bce8ccb855468ca1a064b820610c2469716d481
maya <maya.romm0401@gmail.com>
2025-08-05 17:09:13 +0300

    fixing

--- exceptions/Exceptions.java
+++ exceptions/Exceptions.java
@@ -10,7 +10,7 @@ public class Exceptions {
     public static void makeExceptions() {
         RuntimeException runtimeException = new RuntimeException("bla bla");
         IndexOutOfBoundsException indexOutOfBoundsException = new IndexOutOfBoundsException("bla bla2");
-        System.out.println(indexOutOfBoundsException.getStackTrace());
+        indexOutOfBoundsException.printStackTrace();
         throw runtimeException;
     }
 
