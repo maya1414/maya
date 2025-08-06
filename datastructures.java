3ae0fe76cdfcac116435ef9fc44fdca35c53dcf6
maya <maya.romm0401@gmail.com>
2025-08-06 19:00:45 +0300

    2

--- datastructures.java
+++ datastructures.java
@@ -2,17 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class datastructures {
-    public static void sortingNumbers(){
+    public static void sortingNumbers() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter");
         String num = input.next();
-        ArrayList arr = new ArrayList();
-        while (num != "q"){
+        ArrayList<Number> arr = new ArrayList<>();
+        ArrayList<Number> arr2 = new ArrayList<>();
+        while (!num.equals("q")) {
             double number = Double.parseDouble(num);
-            if (number % 10 == 0){
+            if (number % 10 == 0) {
                 arr.add(number);
             }
+            arr2.add(number);
+            num = input.next();
         }
+        System.out.print("Enter number to check ");
+        Number check = Double.parseDouble(input.next());
+        boolean numberWasEntered = false;
+        for (Number number : arr2) {
+            if (number.equals(check)) {
+                numberWasEntered = true;
+            }
+        }
+        System.out.println(numberWasEntered);
         System.out.println(arr);
     }
 }
